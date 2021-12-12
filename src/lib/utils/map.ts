3d745d1b1e25342ac42ed9b024a049dcd33d1cc8
Adrian Mroz <adrianmroz@gmail.com>
2021-12-12 18:43:01 +0100

    Infer Map keys

--- src/lib/utils/map.ts
+++ src/lib/utils/map.ts
@@ -1,10 +1,5 @@
 import type { Unary } from "./function-types";
 
-function mapRecord<T, R, K extends keyof T>(coll: Record<K, T>, fn: Unary<T, R>): Record<K, R> {
-  return Object.keys(coll)
-    .reduce((o, key) =>
-      Object.assign(o, { [key]: fn(coll[key as K]) }), {} as Record<K, R>);
-}
 
 const asArrayBrand = Symbol("svelte-viz-as-array-brand");
 
@@ -22,16 +17,22 @@ export function asArray<T>(value: T[]): AsArray<T> {
 
 const asMapBrand = Symbol("svelte-viz-as-map-brand");
 
-export interface AsMap<T> {
+export interface AsMap<T, K extends PropertyKey> {
   readonly __brand: typeof asMapBrand;
-  value: Record<string, T>; // TODO: fix string
+  value: Record<K, T>;
 }
 
-export function asMap<T>(value: Record<string, T>): AsMap<T> {
+export function asMap<T, K extends PropertyKey>(value: Record<K, T>): AsMap<T, K> {
   return {
     __brand: asMapBrand,
     value
   };
+}
+
+function mapRecord<T, R, K extends keyof T>(coll: Record<K, T>, fn: Unary<T, R>): Record<K, R> {
+  return Object.keys(coll)
+    .reduce((o, key) =>
+      Object.assign(o, { [key]: fn(coll[key as K]) }), {} as Record<K, R>);
 }
 
 export function map<T, R>(value: T, fn: Unary<Extract<T>, R>): ExtractValuesAs<T, R>;
@@ -43,10 +44,10 @@ export function map(value: any, fn: Unary<unknown, unknown>): unknown {
 
 export type Extract<T> =
   T extends AsArray<infer TS> ? TS :
-    T extends AsMap<infer TS> ? TS :
+    T extends AsMap<infer TS, infer _> ? TS :
       T;
 
 export type ExtractValuesAs<T, R> =
   T extends AsArray<infer _> ? R[] :
-    T extends AsMap<infer _> ? Record<string, R> :
+    T extends AsMap<infer _, infer K> ? Record<K, R> :
       R;
