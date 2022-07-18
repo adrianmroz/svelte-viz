79f5592840c30feacaecd934f9b7ed8c175a57e7
Adrian Mroz <adrianmroz@gmail.com>
2022-07-18 21:39:16 +0200

    disable tailwind inside library code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/components/**/*.{html,js,svelte,ts}', './src/routes/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       fontFamily: {
