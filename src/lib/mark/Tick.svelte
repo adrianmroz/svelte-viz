d131d2d979b3e8bc9f401607391acb9117fbda71
Adrian Mroz <adrianmroz@gmail.com>
2022-07-19 17:43:20 +0200

    redefine ticks interface

--- src/lib/mark/Tick.svelte
+++ src/lib/mark/Tick.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-	export let x;
-	export let y;
-	export let length;
+	export let x1;
+	export let x2;
+	export let y1;
+	export let y2;
 	export let width = 0.5;
-	export let horizontal = false;
 </script>
 
-<line
-	stroke="currentColor"
-	stroke-width={width}
-	x1={x}
-	y1={y}
-	x2={horizontal ? x + length : x}
-	y2={horizontal ? y : y + length}
-/>
+<line stroke="currentColor" stroke-width={width} {x1} {y1} {x2} {y2} />
